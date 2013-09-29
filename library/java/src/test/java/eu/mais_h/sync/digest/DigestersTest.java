5d5d96bf1d14f79f4c58024ba27e536f5c1fe39a
Mathieu Bruyen <code@mais-h.eu>
2013-09-29 15:21:24 +0200

    Move to fest assertions

--- library/java/src/test/java/eu/mais_h/sync/digest/DigestersTest.java
+++ library/java/src/test/java/eu/mais_h/sync/digest/DigestersTest.java
@@ -1,7 +1,6 @@
 package eu.mais_h.sync.digest;
 
-import static org.hamcrest.CoreMatchers.sameInstance;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.fest.assertions.Assertions.assertThat;
 
 import org.junit.Test;
 
@@ -9,6 +8,6 @@ public class DigestersTest {
 
   @Test
   public void always_use_same_sha1_digester_instance() {
-    assertThat(Digesters.sha1(), sameInstance(Digesters.sha1()));
+    assertThat(Digesters.sha1()).isSameAs(Digesters.sha1());
   }
 }

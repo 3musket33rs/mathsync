303073ad929697ae0992d11c0d57f77028a3ec97
unknown <mbruyen@NCEL4076.iis.amadeus.net>
2013-10-02 13:22:09 +0200

    Javascript skeleton

---
+++ library/javascript/test/basic.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+var sync = require('../src');
+
+describe('Basic', function(){
+  describe('return value', function(){
+    it('should return value for key', function(){
+      assert.equal('value', sync.key);
+    })
+  })
+})

2b76ff84ad159f8e5c4f86f0e42d0ad5fc3536d3
Mathieu Bruyen <code@mais-h.eu>
2013-10-02 13:47:23 +0200

    Browserify content

--- library/javascript/Gruntfile.js
+++ library/javascript/Gruntfile.js
@@ -2,13 +2,21 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+    pkg : grunt.file.readJSON('package.json'),
+    jshint : {
+      all : [ 'Gruntfile.js', 'src/**/*.js', 'test/**/*.js' ]
+    },
+    browserify : {
+      dist  : {
+        files: {
+          'build/browser.js': 'src/**/*.js'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-browserify');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'browserify']);
 };

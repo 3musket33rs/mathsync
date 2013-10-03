e0839da9aaeb1ab7b125e4e0d31ec264b2a7f7fc
Mathieu Bruyen <code@mais-h.eu>
2013-10-03 14:04:04 +0200

    Add item to buffers

--- library/javascript/src/ibf.js
+++ library/javascript/src/ibf.js
@@ -1,23 +1,60 @@
 (function () {
   'use strict';
 
-  function initBuffers(size) {
-    var buffers = [];
-    for (var i = 0; i < size; i++) {
-      buffers.push({ items : 0, hashed : [], xored : [] });
+  function arrayCopy(array) {
+    var copy = [];
+    for (var i = 0; i < array.length; i++) {
+      copy.push(array[i]);
     }
-    return buffers;
+    return copy;
   }
 
-  function ibf(size) {
-    var buffers = initBuffers(size);
+  function xorArrays(array1, array2) {
+    var l = Math.max(array1.length, array2.length);
+    var v1;
+    var v2;
+    var xored = [];
+    for (var i = 0; i < l; i++) {
+      v1 = (array1[i] || 0) & 0xff;
+      v2 = (array2[i] || 0) & 0xff;
+      xored.push(v1 ^ v2);
+    }
+    return xored;
+  }
+
+  function initBuckets(size) {
+    var buckets = [];
+    for (var i = 0; i < size; i++) {
+      buckets.push({ items : 0, hashed : [], xored : [] });
+    }
+    return buckets;
+  }
+
+  function ibf(size, spread, digest) {
+    var buckets = initBuckets(size);
 
     function toJson() {
-      return buffers;
+      return buckets;
+    }
+
+    function addItem(content) {
+      var bucketId;
+      var bucket;
+      var copy = arrayCopy(content);
+      copy.push(0);
+      for (var i = 0; i < spread; i++) {
+        copy[copy.length - 1] = i;
+        bucketId = digest(copy)[0] % size;
+        bucket = buckets[bucketId];
+        bucket.items++;
+        bucket.xored = xorArrays(bucket.xored, content);
+        bucket.hashed = xorArrays(bucket.hashed, digest(content));
+      }
     }
 
     return {
-      toJson: toJson
+      toJson : toJson,
+      addItem : addItem
     };
   }
 
--- library/javascript/test/ibf.js
+++ library/javascript/test/ibf.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var assert = require('assert');
+  var _ = require('underscore');
   var ibf = require('../src/ibf');
 
   describe('IBF', function() {
@@ -14,6 +15,24 @@
         assert.deepEqual({ items : 0, hashed : [], xored : [] }, i.toJson()[0]);
         assert.deepEqual({ items : 0, hashed : [], xored : [] }, i.toJson()[1]);
         assert.deepEqual({ items : 0, hashed : [], xored : [] }, i.toJson()[2]);
+      });
+      it('should map item to corresponding buffers', function() {
+        var i = ibf(5, 2, function (content) {
+          if (_.isEqual(content, [ 3, 4, 0])) {
+            return [ 2, 10 ];
+          } else if (_.isEqual(content, [ 3, 4, 1])) {
+            return [ 9, 11 ];
+          } else if (_.isEqual(content, [ 3, 4 ])) {
+            return [ 8, 9 ];
+          }
+        });
+        i.addItem([ 3, 4 ]);
+
+        assert.deepEqual({ items : 0, hashed : [], xored : [] }, i.toJson()[0]);
+        assert.deepEqual({ items : 0, hashed : [], xored : [] }, i.toJson()[1]);
+        assert.deepEqual({ items : 1, hashed : [ 8, 9], xored : [ 3, 4] }, i.toJson()[2]);
+        assert.deepEqual({ items : 0, hashed : [], xored : [] }, i.toJson()[3]);
+        assert.deepEqual({ items : 1, hashed : [ 8, 9], xored : [ 3, 4] }, i.toJson()[4]);
       });
     });
   });

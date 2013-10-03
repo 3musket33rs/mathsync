19419e58bf2abe9734ea5f67155506053db4d52d
Mathieu Bruyen <code@mais-h.eu>
2013-10-03 13:18:27 +0200

    Ibf initializes its buffers

--- library/javascript/test/ibf.js
+++ library/javascript/test/ibf.js
@@ -6,5 +6,11 @@ describe('IBF', function(){
     it('should have lenght corresponding to its size', function(){
       assert.equal(5, ibf(5).toJson().length);
     });
+    it('should have initialize buffers', function(){
+      var i = ibf(3);
+      assert.deepEqual({ items : 0, hashed : [], xored : [] }, i.toJson()[0]);
+      assert.deepEqual({ items : 0, hashed : [], xored : [] }, i.toJson()[1]);
+      assert.deepEqual({ items : 0, hashed : [], xored : [] }, i.toJson()[2]);
+    });
   });
 });

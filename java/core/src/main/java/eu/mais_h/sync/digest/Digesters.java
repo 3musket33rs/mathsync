842668f19ea09f97792b494ad85377aa4789ef5f
Mathieu Bruyen <code@mais-h.eu>
2013-10-26 11:04:22 +0200

    Digester contract in the javadoc

--- java/core/src/main/java/eu/mais_h/sync/digest/Digesters.java
+++ java/core/src/main/java/eu/mais_h/sync/digest/Digesters.java
@@ -1,5 +1,8 @@
 package eu.mais_h.sync.digest;
 
+/**
+ * Common {@link Digester} implementations.
+ */
 public class Digesters {
 
   private static final Digester SHA1 = new Sha1Digester();
@@ -8,6 +11,12 @@ public class Digesters {
     throw new AssertionError();
   }
 
+  /**
+   * Retrieves a digester producing SHA-1 digests.
+   * 
+   * @return a digester producing SHA-1 digests.
+   * @see <a href="http://fr.wikipedia.org/wiki/SHA-1">Wikipedia's article on SHA-1</a>.
+   */
   public static Digester sha1() {
     return SHA1;
   }
--- java/core/src/main/java/eu/mais_h/sync/digest/package-info.java
+++ java/core/src/main/java/eu/mais_h/sync/digest/package-info.java
@@ -1,4 +1,8 @@
 /**
  * Digesting utilities.
+ * 
+ * <p>Common implementations are provided by {@link eu.mais_h.sync.digest.Digesters}.</p>
+ * 
+ * <p>Custom implementations can be defined by implementing {@link eu.mais_h.sync.digest.Digester}.</p>
  */
 package eu.mais_h.sync.digest;

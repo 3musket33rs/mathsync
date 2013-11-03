4746653d2f82be920c56cc5237c8e6d4b7dd66bf
Mathieu Bruyen <code@mais-h.eu>
2013-11-03 17:15:54 +0100

    Tests for resolving difference from local and remote summarizers

--- java/core/src/main/java/eu/mais_h/sync/ResolverFromSummarizers.java
+++ java/core/src/main/java/eu/mais_h/sync/ResolverFromSummarizers.java
@@ -1,13 +1,13 @@
 package eu.mais_h.sync;
 
 
-class ResolverFromCompressors<T> implements Resolver<T> {
+class ResolverFromSummarizers<T> implements Resolver<T> {
 
   private final Summarizer remote;
   private final Summarizer local;
   private final Deserializer<T> deserializer;
 
-  ResolverFromCompressors(Summarizer remote, Summarizer local, Deserializer<T> deserializer) {
+  ResolverFromSummarizers(Summarizer remote, Summarizer local, Deserializer<T> deserializer) {
     this.remote = remote;
     this.local = local;
     this.deserializer = deserializer;
@@ -15,7 +15,7 @@ class ResolverFromCompressors<T> implements Resolver<T> {
 
   @Override
   public Difference<T> difference() {
-    int level = 1;
+    int level = 0;
     Difference<byte[]> difference = null;
     while (difference == null) {
       level++;
@@ -28,10 +28,10 @@ class ResolverFromCompressors<T> implements Resolver<T> {
 
   private Difference<byte[]> computeDifference(Summary remoteIbf, Summary localIbf) {
     if (!(remoteIbf instanceof Ibf)) {
-      throw new IllegalArgumentException("Remote summary has an invalid type: " + remoteIbf);
+      throw new IllegalStateException("Remote summary has an invalid type: " + remoteIbf);
     }
     if (!(localIbf instanceof Ibf)) {
-      throw new IllegalArgumentException("Local summary has an invalid type: " + localIbf);
+      throw new IllegalStateException("Local summary has an invalid type: " + localIbf);
     }
     return ((Ibf)localIbf).substract((Ibf)remoteIbf).asDifference();
   }
--- java/core/src/main/java/eu/mais_h/sync/Resolvers.java
+++ java/core/src/main/java/eu/mais_h/sync/Resolvers.java
@@ -7,4 +7,8 @@ public class Resolvers {
   private Resolvers() {
     throw new AssertionError();
   }
+  
+  public <T> Resolver<T> fromSummarizers(Summarizer remote, Summarizer local, Deserializer<T> deserializer) {
+    return new ResolverFromSummarizers<>(remote, local, deserializer);
+  }
 }

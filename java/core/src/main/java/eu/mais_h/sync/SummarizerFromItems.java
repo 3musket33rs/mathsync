16061e1ffdbd1127cb0ec87e099df094014cd647
Mathieu Bruyen <code@mais-h.eu>
2013-12-12 09:55:40 +0100

    Recommend using odd spread

--- java/core/src/main/java/eu/mais_h/sync/SummarizerFromItems.java
+++ java/core/src/main/java/eu/mais_h/sync/SummarizerFromItems.java
@@ -10,7 +10,7 @@ import eu.mais_h.sync.serialize.Serializer;
  * Summarizer which iterates on items in the current state to build summaries.
  */
 public class SummarizerFromItems implements Summarizer {
-  
+
   private final Iterable<byte[]> items;
   private final Digester digester;
   private final int spread;
@@ -23,7 +23,7 @@ public class SummarizerFromItems implements Summarizer {
 
   /**
    * {@inheritDoc}
-   * 
+   *
    * <p>Summaries produced have a size of <code>O(2^level)</code>.</p>
    */
   @Override
@@ -41,22 +41,22 @@ public class SummarizerFromItems implements Summarizer {
 
   /**
    * Builds a simple summarizer.
-   * 
+   *
    * @param items the set of containing all items in the current state.
    * @param serializer the serializer to use to serialize items.
    * @return a summarizer with {@link Sha1Digester SHA-1 digester} and default spread.
    */
   public static <T> Summarizer simple(Set<? extends T> items, Serializer<? super T> serializer) {
-    return custom(items, serializer, Sha1Digester.get(), 4);
+    return custom(items, serializer, Sha1Digester.get(), 3);
   }
 
   /**
    * Builds a custom summarizer.
-   * 
+   *
    * @param items the set of containing all items in the current state.
    * @param serializer the serializer to use to serialize items.
    * @param digester the custom digester to use.
-   * @param spread the number of buckets to store each item in.
+   * @param spread the number of buckets to store each item in, it is recommended using an odd number to prevent items falling twice in the same bucket to become invisible.
    * @return a summarizer corresponding to the input.
    */
   public static <T> Summarizer custom(Set<? extends T> items, Serializer<? super T> serializer, Digester digester, int spread) {

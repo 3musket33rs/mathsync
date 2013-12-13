6d3fce7f846d4ac4e900ac8e44cc0c00f9bd0878
corinnekrych <corinnekrych@gmail.com>
2013-12-13 23:29:30 +0100

    add project lib structure for ios

---
+++ ios/core/core/core.h
@@ -0,0 +1,13 @@
+//
+//  core.h
+//  core
+//
+//  Created by Corinne Krych on 12/13/13.
+//  Copyright (c) 2013 3musket33rs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface core : NSObject
+
+@end

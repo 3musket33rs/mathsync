356afc9a3e4c39313324748908f1700fb2f73184
Mathieu Bruyen <code@mais-h.eu>
2013-12-22 15:17:12 +0100

    Use rusha instead of crypto-js for sha-1 computation
    Uses TypedArrays natively rather than emulated and is said to be
    asm.js compatible.

--- javascript/core/src/sha1.js
+++ javascript/core/src/sha1.js
@@ -1,17 +1,14 @@
 (function () {
   'use strict';
 
-  var digest = require('crypto-js/sha1');
-  var WordArray = require('crypto-js/lib-typedarrays').init;
+  var Rusha = require('rusha');
+  var rusha = new Rusha();
 
   function sha1(source) {
     if (!(source instanceof ArrayBuffer)) {
       throw new TypeError('Cannot digest source: ' + source);
     }
-    var wordArray = new WordArray(source);
-    var digested = digest(wordArray);
-    var int32view = new Int32Array(digested.words);
-    return int32view.buffer;
+    return rusha.rawDigest(source).buffer.slice(0, 20);
   }
 
   module.exports = sha1;

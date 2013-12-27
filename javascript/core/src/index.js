0293bf4fe40f4dd5f9d33d6a740dae209a59fd0f
Mathieu Bruyen <code@mais-h.eu>
2013-12-27 13:19:48 +0000

    Provide default summarizers in main script

--- javascript/core/src/index.js
+++ javascript/core/src/index.js
@@ -1,1 +1,23 @@
-module.exports = { key: 'value' };
+(function () {
+  'use strict';
+
+  var sha1 = require('./sha1');
+  var summarizer = require('./summarizer');
+
+  function fromItems(array, serialize) {
+    return summarizer.fromItems(array, serialize, sha1, 3);
+  }
+
+  function fromJSON(producer) {
+    return summarizer.fromJSON(producer, sha1, 3);
+  }
+
+  module.exports = {
+    summarizer: {
+      fromItems : fromItems,
+      fromJSON : fromJSON,
+      fromLarge : summarizer.fromLarge
+    },
+    resolver: require('./resolver')
+  };
+})();

34cbf1bb7a28bbb2da37dfeedeae312db697ad71
Mathieu Bruyen <code@mais-h.eu>
2014-01-09 17:30:37 +0000

    Javascript default summarizer uses bucket selector

--- javascript/core/src/index.js
+++ javascript/core/src/index.js
@@ -3,13 +3,14 @@
 
   var sha1 = require('./sha1');
   var summarizer = require('./summarizer');
+  var selector = require('./bucketSelector').padAndHash(sha1, 3);
 
   function fromItems(array, serialize) {
-    return summarizer.fromItems(array, serialize, sha1, 3);
+    return summarizer.fromItems(array, serialize, sha1, selector);
   }
 
   function fromJSON(producer) {
-    return summarizer.fromJSON(producer, sha1, 3);
+    return summarizer.fromJSON(producer, sha1, selector);
   }
 
   module.exports = {

e64ca810d02b9f4a71888771ebbe52457f624258
Mathieu Bruyen <code@mais-h.eu>
2014-02-18 21:28:41 +0100

    Document Summary#plusAsync plus some js doc improvements

--- javascript/core/src/index.js
+++ javascript/core/src/index.js
@@ -11,6 +11,68 @@
    */
 
   /**
+   * The native promise object.
+   *
+   * @external Promise
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise Promise on Mozilla Developer Network}
+   */
+  /**
+   * Register fullfillment and rejection handlers to the promise.
+   *
+   * @param {function} onFullfilled - fullfillment handler called with the resolved value.
+   * @param {function} onRejected - rejection handler called with the rejection cause.
+   * @returns {external:Promise} a promise resolving to the result value of the called handler.
+   * @function external:Promise#then
+   */
+
+  /**
+   * An occurrence of a Generator.
+   *
+   * <p>This is the result of calling a generator function which does <code>yield</code> on each item it wants to
+   * expose, or on promise to items it wants to expose. It can actually be a hand made object as long as it respects
+   * the contract of this interface.</p>
+   *
+   * @example
+   * function* generator() {
+   *   yield 1;
+   *   yield 2;
+   *   yield 3;
+   * }
+   * var it = generator();
+   *
+   * @example
+   * var it = (function () {
+   *   var i = 0;
+   *   var content = [1, 2, 3];
+   *   return {
+   *     next : function () {
+   *       var res;
+   *       if (i < content.length) {
+   *         res = { done : false, value : content[i] };
+   *       } else {
+   *         res = { done : true };
+   *       }
+   *       i++;
+   *       return res;
+   *     }
+   *   };
+   * })();
+   *
+   * @external Iterator
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator Generator on Mozilla Developer Network}
+   */
+  /**
+   * Reads the next item on the iterator.
+   *
+   * <p>Can either return a value if it is not done, <code>{ done : false, value : 'some value' }</code>, or inform it
+   * is done with <code>{ done : true }</code>. <code>value</code> can either be the actual value or a
+   * {@link external:Promise promise} which will resolve to the desired value.</p>
+   *
+   * @returns Object a done marker, or a value container, or a promise-for-value container.
+   * @function external:Iterator#next
+   */
+
+  /**
    * Represents summarized data.
    * @external Summary
    */
@@ -30,9 +92,19 @@
    * @function external:Summary#plus
    */
   /**
+   * Adds several items to the summary.
+   *
+   * <p>Equivalent to repeatedly calling {@link external:Summary#plus} for each element, but this
+   * method can do optimizations for batch updates.</p>
+   *
+   * @param {external:Iterator} iterator - an iterator of items to add.
+   * @returns {external:Promise.<external:Summary>} a promise which will resolve to a summary.
+   * @function external:Summary#plusAsync
+   */
+  /**
    * Retrieves a view of the summary as a difference.
    *
-   * @returns {external:Difference.<ArrayBuffer>} a difference view of the summary or <code>null</code> if it cannot be resolved with the information it contains.
+   * @returns {external:Difference.<external:ArrayBuffer>} a difference view of the summary or <code>null</code> if it cannot be resolved with the information it contains.
    * @function external:Summary#toDifference
    */
   /**

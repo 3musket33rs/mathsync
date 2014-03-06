f1355b686dba4be86fd433f7c4861e4e3ffe44e5
Mathieu Bruyen <code@mais-h.eu>
2014-03-06 19:52:49 +0100

    Factorized resolver algorithm in javascript

--- javascript/core/src/resolver.js
+++ javascript/core/src/resolver.js
@@ -12,56 +12,54 @@
   var q = require('q');
   var iterator = require('./iterator');
 
-  function deserializeDifference(deserialize, difference) {
-    var deserialized = { added : [], removed : [] };
-    var i;
-    for (i = 0; i < difference.added.length; i++) {
-      deserialized.added.push(deserialize(difference.added[i]));
+  function iterateOnLevelAndDeserialize(compute, deserialize) {
+
+    function fetchDifference(level) {
+      return compute(level).then(function (diff) {
+        if (diff === null) {
+          return fetchDifference(level + 1);
+        } else {
+          return diff;
+        }
+      });
     }
-    for (i = 0; i < difference.removed.length; i++) {
-      deserialized.removed.push(deserialize(difference.removed[i]));
+
+    function deserializeDifference(difference) {
+      var deserialized = { added : [], removed : [] };
+      var i;
+      for (i = 0; i < difference.added.length; i++) {
+        deserialized.added.push(deserialize(difference.added[i]));
+      }
+      for (i = 0; i < difference.removed.length; i++) {
+        deserialized.removed.push(deserialize(difference.removed[i]));
+      }
+      return deserialized;
     }
-    return deserialized;
+
+    function resolve() {
+      return fetchDifference(0).then(deserializeDifference);
+    }
+
+    return resolve;
   }
 
   function fromItems(array, remote, serialize, deserialize) {
-    function fetchDifference(level) {
+    return iterateOnLevelAndDeserialize(function (level) {
       return remote(level).then(function (summary) {
         var it = iterator.map(iterator.fromArray(array), serialize);
         return summary.minusIterator(it);
       }).then(function (summary) {
         return summary.toDifference();
-      }).then(function (diff) {
-        if (diff === null) {
-          return fetchDifference(level + 1);
-        } else {
-          return diff;
-        }
       });
-    }
-
-    return function () {
-      return fetchDifference(0).then(deserializeDifference.bind(null, deserialize));
-    };
+    }, deserialize);
   }
 
   function fromSummarizers(local, remote, deserialize) {
-
-    function fetchDifference(level) {
+    return iterateOnLevelAndDeserialize(function (level) {
       return q.all([local(level), remote(level)]).then(function (arr) {
         return arr[1].minus(arr[0]).toDifference();
-      }).then(function (diff) {
-        if (diff === null) {
-          return fetchDifference(level + 1);
-        } else {
-          return diff;
-        }
       });
-    }
-
-    return function () {
-      return fetchDifference(0).then(deserializeDifference.bind(null, deserialize));
-    };
+    }, deserialize);
   }
 
   /**
@@ -90,7 +88,6 @@
      * @param {serialize} serialize - a serializer for items in the array.
      * @param {deserialize} deserialize - how to deserialize byte arrays to objects.
      * @return {resolver} a resolver returning differences between the summarizer and local items.
-     * @deprecated
      */
     fromItems : fromItems
   };

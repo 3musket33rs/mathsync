4fc81bf0629535bfe2c010cefd8736913df79e36
Mathieu Bruyen <code@mais-h.eu>
2014-03-06 19:13:25 +0100

    Actually test resolver from summarizers in javascript

--- javascript/core/test/resolver.js
+++ javascript/core/test/resolver.js
@@ -1,56 +1,38 @@
 (function () {
   'use strict';
 
-  var assert = require('assert');
-  var _ = require('underscore');
   var resolver = require('../src/resolver');
   var summarizer = require('../src/summarizer');
   var sha1 = require('../src/sha1');
+  var bucketSelector = require('../src/bucketSelector').padAndHash(sha1, 3);
+  var assertThatSetOfArrayEquals = require('./utils').assertThatSetOfArrayEquals;
 
-  function assertThatSetOfArrayEquals(arr1, arr2) {
-    assert.equal(arr1.lenght, arr2.lenght);
-    assert.ok(_(arr1).every(function (item1) {
-      return _(arr2).some(function (item2) {
-        return _.isEqual(item1, item2);
-      });
-    }));
+  function serialize(value) {
+    return new Int8Array(value).buffer;
+  }
+  function deserialize(value) {
+    var view = new Int8Array(value);
+    var res = [];
+    for (var i = 0; i < view.length; i++) {
+      res.push(view[i]);
+    }
+    return res;
   }
 
   describe('Resolver', function() {
     describe('fromSummarizers', function() {
       it('generate difference', function(done) {
-        function serialize(value) {
-          return new Int8Array(value).buffer;
-        }
-        function deserialize(value) {
-          var view = new Int8Array(value);
-          var res = [];
-          for (var i = 0; i < view.length; i++) {
-            res.push(view[i]);
-          }
-          return res;
-        }
+        var localItems = [[1, 2], [2, 2], [3, 2]];
+        var local = summarizer.fromItems(localItems, serialize, sha1, bucketSelector);
 
-        function* localItems() {
-          yield [1, 2];
-          yield [2, 2];
-          yield [3, 2];
-        }
-        var local = summarizer.fromItems(localItems, serialize, sha1, 4);
-
-        function* remoteItems() {
-          yield [1, 2];
-          yield [4, 2];
-        }
-        var remote = summarizer.fromItems(remoteItems, serialize, sha1, 4);
+        var remoteItems = [[1, 2], [4, 2]];
+        var remote = summarizer.fromItems(remoteItems, serialize, sha1, bucketSelector);
 
         resolver.fromSummarizers(local, remote, deserialize)().then(function (difference) {
           assertThatSetOfArrayEquals(difference.added, [[4, 2]]);
           assertThatSetOfArrayEquals(difference.removed, [[2, 2], [3, 2]]);
           done();
-        }, function (err) {
-          done(err);
-        });
+        }, done);
       });
     });
   });

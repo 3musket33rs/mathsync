98b5beee3e1b530c14108ce0c3262b338fd635e0
Mathieu Bruyen <code@mais-h.eu>
2014-03-18 08:43:53 +0100

    Use ad-hoc library for base64 encoding
    Uses npm:base64-arraybuffer library, inlined because latest fixes
    have not been published to npm yet.

--- javascript/core/src/arrayBufferSerialization.js
+++ javascript/core/src/arrayBufferSerialization.js
@@ -1,18 +1,88 @@
 (function () {
   'use strict';
 
-  var Buffer = require('buffer').Buffer;
+  /*
+   * base64-arraybuffer
+   * https://github.com/niklasvh/base64-arraybuffer
+   *
+   * Copyright (c) 2012 Niklas von Hertzen
+   * Licensed under the MIT license.
+   */
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
-  function toString(arr) {
-    return new Buffer(new Int8Array(arr)).toString('base64');
+  function encode(arraybuffer) {
+    var bytes = new Uint8Array(arraybuffer),
+    i, len = bytes.length, base64 = '';
+
+    for (i = 0; i < len; i+=3) {
+      base64 += chars[bytes[i] >> 2];
+      base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
+      base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
+      base64 += chars[bytes[i + 2] & 63];
+    }
+
+    if ((len % 3) === 2) {
+      base64 = base64.substring(0, base64.length - 1) + '=';
+    } else if (len % 3 === 1) {
+      base64 = base64.substring(0, base64.length - 2) + '==';
+    }
+
+    return base64;
   }
 
-  function fromString(str) {
-    return new Int8Array(new Buffer(str, 'base64').toJSON().data).buffer;
+  function decode(base64) {
+    var bufferLength = base64.length * 0.75,
+    len = base64.length, i, p = 0,
+    encoded1, encoded2, encoded3, encoded4;
+
+    if (base64[base64.length - 1] === '=') {
+      bufferLength--;
+      if (base64[base64.length - 2] === '=') {
+        bufferLength--;
+      }
+    }
+
+    var arraybuffer = new ArrayBuffer(bufferLength),
+    bytes = new Uint8Array(arraybuffer);
+
+    for (i = 0; i < len; i+=4) {
+      encoded1 = chars.indexOf(base64[i]);
+      encoded2 = chars.indexOf(base64[i+1]);
+      encoded3 = chars.indexOf(base64[i+2]);
+      encoded4 = chars.indexOf(base64[i+3]);
+
+      bytes[p++] = (encoded1 << 2) | (encoded2 >> 4);
+      bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
+      bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63);
+    }
+
+    return arraybuffer;
   }
 
+  /**
+   * Serializes and deserializes byte arrays to and from strings.
+   *
+   * @see {@link https://www.npmjs.org/package/base64-arraybuffer|base64-arraybuffer, inlined because latest version is not published}
+   * @module arrayBufferSerialization
+   */
   module.exports = {
-    toString: toString,
-    fromString: fromString
+
+    /**
+     * Serializes an ArrayBuffer to a string.
+     *
+     * @function
+     * @param {ArrayBuffer} buffer - the buffer to serialize.
+     * @return {string} a string representing buffer content.
+     */
+    toString: encode,
+
+    /**
+     * Deserializes a string to an ArrayBuffer
+     *
+     * @function
+     * @param {string} content - the serialized string to decode.
+     * @return {ArrayBuffer} a buffer corresponding to the string.
+     */
+    fromString: decode
   };
 })();

fb65ad3229edaf241e0791d30011fa7206c916d8
Mathieu Bruyen <code@mais-h.eu>
2014-03-28 12:43:42 +0000

    mocha 1.18 for integration tests

--- integration-test/test.js
+++ integration-test/test.js
@@ -8,33 +8,33 @@
   var client;
   var server;
 
-  before(function (done) {
+  before(function () {
     var port = 12003;
-    startServer(port).then(function (s) {
+    return startServer(port).then(function (s) {
       server = s;
       return startClient(port);
     }).then(function (c) {
       client = c;
-    }).then(done, done);
+    });
   });
 
-  beforeEach(function (done) {
-    server.clear().then(client.clear).then(function () {}).then(done, done);
+  beforeEach(function () {
+    return server.clear().then(client.clear);
   });
 
-  after(function (done) {
-    client.stop().then(server.stop).then(done, done);
+  after(function () {
+    return client.stop().then(server.stop);
   });
 
   describe('Synchronization', function() {
 
-    it('should handle added item', function(done) {
-      server.put('key', 'value').then(client.sync).then(client.get).then(function (state) {
+    it('should handle added item', function() {
+      return server.put('key', 'value').then(client.sync).then(client.get).then(function (state) {
         assert.equal('value', state.key);
-      }).then(done, done);
+      });
     });
 
-    it('should handle many added item', function(done) {
+    it('should handle many added item', function() {
       var i;
       var items = 1000;
       var p = server.put('key0', 'value0');
@@ -45,27 +45,27 @@
           });
         })(i);
       }
-      p.then(client.sync).then(client.get).then(function (state) {
+      return p.then(client.sync).then(client.get).then(function (state) {
         for (i = 0; i < items; i++) {
           assert.equal('value' + i, state['key' + i]);
         }
-      }).then(done, done);
+      });
     });
 
-    it('should handle removed item', function(done) {
-      server.put('key', 'value').then(client.sync).then(function () {
+    it('should handle removed item', function() {
+      return server.put('key', 'value').then(client.sync).then(function () {
         return server.del('key');
       }).then(client.sync).then(client.get).then(function (state) {
         assert.equal(undefined, state.key);
-      }).then(done, done);
+      });
     });
 
-    it('should handle modified item', function(done) {
-      server.put('key', 'value1').then(client.sync).then(function () {
+    it('should handle modified item', function() {
+      return server.put('key', 'value1').then(client.sync).then(function () {
         return server.put('key', 'value2');
       }).then(client.sync).then(client.get).then(function (state) {
         assert.equal('value2', state.key);
-      }).then(done, done);
+      });
     });
   });
 })();

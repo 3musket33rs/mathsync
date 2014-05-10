3e256c6a46a013f7ddff634559d5720760912a13
Mathieu Bruyen <code@mais-h.eu>
2014-05-10 10:38:44 +0200

    Undocument array buffer serialization

--- javascript/core/src/arrayBufferSerialization.js
+++ javascript/core/src/arrayBufferSerialization.js
@@ -3,19 +3,13 @@
 
   var b64 = require('base64-arraybuffer');
 
-  /**
-   * Serializes and deserializes byte arrays to and from strings.
-   *
-   * @see {@link https://www.npmjs.org/package/base64-arraybuffer|base64-arraybuffer, inlined because latest version is not published}
-   * @module arrayBufferSerialization
-   */
   module.exports = {
 
     /**
      * Serializes an ArrayBuffer to a string.
      *
      * @function
-     * @param {ArrayBuffer} buffer - the buffer to serialize.
+     * @param {external:ArrayBuffer} buffer - the buffer to serialize.
      * @return {string} a string representing buffer content.
      */
     toString: b64.encode,
@@ -25,7 +19,7 @@
      *
      * @function
      * @param {string} content - the serialized string to decode.
-     * @return {ArrayBuffer} a buffer corresponding to the string.
+     * @return {external:ArrayBuffer} a buffer corresponding to the string.
      */
     fromString: b64.decode
   };

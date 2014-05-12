910504b711abfe1546847e438845b37e59984295
Mathieu Bruyen <code@mais-h.eu>
2014-05-12 11:54:14 +0000

    Local node modules on PATH on dev machine

--- dev-machine/user.sh
+++ dev-machine/user.sh
@@ -5,6 +5,7 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 source ~/.nvm/nvm.sh
 nvm install 0.11
 nvm alias default 0.11
+echo 'export PATH=$PATH:node_modules/.bin' >> ~/.bashrc
 
 # Maven
 wget -O /tmp/maven.tar.gz http://mir2.ovh.net/ftp.apache.org/dist/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz
